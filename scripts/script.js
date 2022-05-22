4c169320ac372e34de7004639ccff0a61358137f
Micho-PC <michelhdd27@gmail.com>
2022-05-23 00:40:10 +0300

    added start and random function

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,57 @@
-var audio_y = new Audio("./sounds/yellow.mp3");
-var audio_gr = new Audio("./sounds/green.mp3");
-var audio_bl = new Audio("./sounds/blue.mp3");
-var audio_red = new Audio("./sounds/red.mp3");
-var audio_wrong = new Audio("./sounds/wrong.mp3");
+//Audio files
+var audioGreen = new Audio("./sounds/green.mp3");
+var audioRed = new Audio("./sounds/red.mp3");
+var audioYellow = new Audio("./sounds/yellow.mp3");
+var audioBlue = new Audio("./sounds/blue.mp3");
+var audioWrong = new Audio("./sounds/wrong.mp3");
 
+//buttons
+const buttonGreen  = document.getElementById("green");
+const buttonRed  = document.getElementById("red");
+const buttonYellow  = document.getElementById("yellow");
+const buttonBlue  = document.getElementById("blue");
 
+var array =[]
+var player_array = []
 
-document.getElementById("green").addEventListener("click",() => {
+/* document.getElementById("green").addEventListener("click",() => {
     audio_gr.play();
 });
+ */
+
+document.addEventListener("keydown",start);
+
+function start(){
+    console.log("start is working");
+    //add random number to array function
+    randomButton();
+}
 
 
+function randomButton(){
+    console.log("random is working");
+    let randomNumber = Math.floor(Math.random() * 4) + 1;
+    if (randomNumber==1){
+        audioGreen.play();
+        changeOpacity(buttonGreen);
+    }
+    else if (randomNumber==2){
+        audioRed.play();
+        changeOpacity(buttonRed);
+    }
+    else if (randomNumber==3){
+        audioYellow.play();
+        changeOpacity(buttonYellow);
+    }
+    else {
+        audioBlue.play();
+        changeOpacity(buttonBlue);
+    }
+}
+
+function changeOpacity(btn){
+    btn.style.opacity = "0.2";
+    setTimeout(() =>{
+        btn.style.opacity = "1";
+    }, 200);
+}
